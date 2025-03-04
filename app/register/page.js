8fd89d7ff399533a8b7c7c12f4c822a0bd20aaa6
LQT Dead Meme <luongquoctoan.ks@gmail.com>
2025-03-05 00:34:49 +0700

    feat: update  register ui

--- app/register/page.js
+++ app/register/page.js
@@ -14,6 +14,9 @@ import {
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
+import { Bird, Facebook } from "lucide-react";
+import Image from "next/image";
+import name from "@/public/images/name.png";
 
 // Định nghĩa schema bằng Zod
 const formSchema = z.object({
@@ -39,9 +42,17 @@ export default function RegisterForm() {
   }
 
   return (
-    <div className="flex flex-col items-center justify-center h-screen bg-gray-50">
-      <div className="w-full max-w-md p-8 bg-white rounded shadow">
-        <h1 className="text-3xl font-bold mb-6 text-center">Register</h1>
+    <div className="flex flex-col items-center justify-center h-screen bg-gray-50 bg-[url(/images/bg.png)] bg-no-repeat bg-center bg-cover h-screen w-screen">
+      <div className="w-full max-w-md p-12 bg-baby-powder/90 rounded-xl">
+        <Image
+          className="m-auto"
+          src={name}
+          width={200}
+          height={200}
+          alt="Picture of the author"
+          placeholder="empty"
+        ></Image>
+
         <Form {...form}>
           <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
             {/* Email Field */}
@@ -52,9 +63,13 @@ export default function RegisterForm() {
                 <FormItem>
                   <FormLabel>Email</FormLabel>
                   <FormControl>
-                    <Input placeholder="Your email" {...field} />
+                    <Input
+                      placeholder="Enter your email"
+                      {...field}
+                      className="rounded-sm h-11 my-1 bg-white placeholder:text-sm placeholder:text-sl-gray"
+                    />
                   </FormControl>
-                  <FormMessage className="mt-1 text-sm text-yellow-orange" />
+                  <FormMessage />
                 </FormItem>
               )}
             />
@@ -68,7 +83,8 @@ export default function RegisterForm() {
                   <FormControl>
                     <Input
                       type="password"
-                      placeholder="Your password"
+                      placeholder="Create password"
+                      className="rounded-sm h-11 my-1 bg-white placeholder:text-sm placeholder:text-sl-gray"
                       {...field}
                     />
                   </FormControl>
@@ -76,11 +92,42 @@ export default function RegisterForm() {
                 </FormItem>
               )}
             />
-            <Button type="submit" className="w-full">
-              Register
+            <h3 className="text-xs text-center font-normal">
+              By clicking Agree & Join, you agree to the The Pet{" "}
+              <b>User Agreement, Privacy Policy,</b> and <b>Cookie Policy</b>.
+            </h3>
+            <Button
+              type="submit"
+              className="w-full rounded-full p-5 bg-yellow-orange"
+            >
+              Agree & Join
             </Button>
           </form>
         </Form>
+
+        <div className="flex items-center my-4">
+          <hr className="flex-1 border-l-gray  border-1" />
+          <span className="px-3 text-d-gray text-sm">Or sign in with</span>
+          <hr className="flex-1 border-l-gray border-1" />
+        </div>
+
+        <div className="flex justify-between">
+          <Button className="rounded-full bg-baby-powder border-1 border-jet text-jet hover:bg-l-gray hover:text-white hover:border-none">
+            <Bird fill="black"></Bird> Google
+          </Button>
+          <Button className="rounded-full bg-baby-powder border-1 border-jet text-jet hover:bg-l-gray hover:text-white hover:border-none">
+            <Facebook fill="black"></Facebook> Facebook
+          </Button>
+          <Button className="rounded-full bg-baby-powder border-1 border-jet text-jet hover:bg-l-gray hover:text-white hover:border-none">
+            <Bird fill="black"></Bird> Twitter
+          </Button>
+        </div>
+
+        <div className="text-center mt-7">
+          <h3 className="text-sm text-jet">
+            Already on The Pet? <b className="underline">Sign in</b>
+          </h3>
+        </div>
       </div>
     </div>
   );
